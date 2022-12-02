1f86e04032692785b65016536f4b84d76f04b991
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-02 23:29:58 +0800

    resolving timer bug

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,9 +1,8 @@
-var startBtn = document.getElementsByClassName("start-btn");
-var timeEl = document.getElementsByClassName("time");
+var startBtn = document.querySelector(".start-btn");
+var timeEl = document.querySelector(".time");
 
 console.log(startBtn)
 
-var seconds = "";
 
 var secondsLeft = 100;
 
@@ -12,14 +11,17 @@ function setTime() {
     secondsLeft--;
     timeEl.textContent = "Timer: " + secondsLeft;
 
-        if(secondsLeft === 0) {
+    if(secondsLeft === 0) {
 
-        clearInterval(timerInterval)
-        timeEl.textContent = "You have run out of time! ";
+        clearInterval(timerInterval);
+        timeEl.textContent = "You have run out of time!";
 
-        }
+    }
 
   }, 1000);
 }
+
+
+
+
 startBtn.addEventListener("click", setTime);
-// bug within this event listener - (not a function?)
