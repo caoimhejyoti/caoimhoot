2adc4ed3746c9aaa4f5a4524088ecbd003518cfd
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-02 23:38:15 +0800

    prettier

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,27 +1,21 @@
 var startBtn = document.querySelector(".start-btn");
 var timeEl = document.querySelector(".time");
 
-console.log(startBtn)
-
+console.log(startBtn);
 
 var secondsLeft = 100;
 
 function setTime() {
-  var timerInterval = setInterval(function() {
+  var timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = "Timer: " + secondsLeft;
 
-    if(secondsLeft === 0) {
-
-        clearInterval(timerInterval);
-        timeEl.textContent = "You have run out of time!";
-
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      timeEl.textContent = "You have run out of time!";
     }
-
   }, 1000);
 }
 
-
-
-
 startBtn.addEventListener("click", setTime);
+
