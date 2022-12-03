35f11b76c9137510a071b3e96bba7518fc140e42
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-03 18:09:39 +0800

    trying to resolve answer colour bugs

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -7,19 +7,21 @@ let questions = [(document.querySelector(".quiz-item1")),(document.querySelector
 console.log(questions);
 console.log(startBtn);
 
-// initial js stlyes
+
+
+// initial js stlyes - FIXME:
 function homeStyle(questions) {
 for (let i = 0; i < questions.length; i++){
 questions[i].style.display = 'none'
 }
 }
 
-// timer function
+// count down timer function
 function setTime() {
   var timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = "Timer: " + secondsLeft;
-
+    
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
       timeEl.textContent = "You have run out of time!";
@@ -27,26 +29,26 @@ function setTime() {
   }, 1000);
 }
 
-
+// start quiz function - hide welcome header and intro, show quiztime headser and first question.
 function quiz() {
   var intro = document.querySelector(".intro");
   var quiz = document.querySelector(".quiz");
-
-
-  if (quiz.style.display = 'none');
-    intro.style.display = 'none';
-
   
-    for (let i = 0; i < questions.length; i++) {
-      questions[i].style.display = 'block';
-      
-    }
-
-
+  
+  if (quiz.style.display = 'none');
+  intro.style.display = 'none';
+  
+  
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].style.display = 'block';
+    
+  }
+  
 }
 console.log(questions[0]);
 ;
 
+// master function - all functions combined for single eventListener.
 function quizTime () {
   setTime ();
   console.log(setTime());
@@ -54,8 +56,37 @@ function quizTime () {
   quiz ();
   console.log(quiz());
   console.log("quiz is working");
-
+  
 }
 
-// this function will develop, adding more functions to include all effects.
-startBtn.addEventListener("click", quizTime);
+// master event listener attached to the start button.
+startBtn.addEventListener("click", setTime);
+
+// answer buttons
+var rightBtn = document.querySelector("#answer-right");
+var wrongBtn = document.querySelector("#answer-wrong");
+
+// right answers colour changed based on validitiy
+function rightAnswerReact(){
+  if (rightBtn.style.backgroundColor === "#0d324d"){
+  
+  rightBtn.style.backgroundColor = '#green';
+  // rightBtn.setProperty.style.color = '--background-color';
+  console.log("reading right answer");
+}
+}
+
+// FIXME:
+rightBtn.addEventListener("click", rightAnswerReact);
+console.log("test");
+// console.log(rightAnswerReact);
+
+
+// wrong answers colour changed based on validitiy
+function wrongAnswerReact(){
+  wrongBtn.setProperty.style.backgroundColor ='--wrong-color';
+  wrongBtn.setProperty.style.color = '--background-color';
+}
+
+// FIXME:
+wrongBtn.addEventListener("click", wrongAnswerReact); 
