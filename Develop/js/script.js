ad402b6194da9e8d1548cfc20981d331a20df8d2
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-03 19:24:50 +0800

    post askBCS reating questions var

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -3,18 +3,18 @@ var startBtn = document.querySelector(".start-btn");
 var timeEl = document.querySelector(".time");
 var secondsLeft = 100;
 // quiz vars
-let questions = [(document.querySelector(".quiz-item1")),(document.querySelector(".quiz-item2")), (document.querySelector(".quiz-item3")), (document.querySelector(".quiz-item4"))];
-console.log(questions);
+// let questions = [(document.querySelector(".quiz-item1")),(document.querySelector(".quiz-item2")), (document.querySelector(".quiz-item3")), (document.querySelector(".quiz-item4"))];
+// console.log(questions);
 console.log(startBtn);
 
 
 
 // initial js stlyes - FIXME:
-function homeStyle(questions) {
-for (let i = 0; i < questions.length; i++){
-questions[i].style.display = 'none'
-}
-}
+// function homeStyle(questions) {
+// for (let i = 0; i < questions.length; i++){
+// questions[i].style.display = 'none'
+// }
+// }
 
 // count down timer function
 function setTime() {
@@ -46,20 +46,21 @@ function quiz() {
   
 }
 console.log(questions[0]);
-;
 
-// master function - all functions combined for single eventListener.
-function quizTime () {
-  setTime ();
-  console.log(setTime());
-  console.log("set time is working");
-  quiz ();
-  console.log(quiz());
-  console.log("quiz is working");
+
+//FIXME: master function - all functions combined for single eventListener.
+// function quizTime () {
+//   setTime ();
+//   console.log(setTime());
+//   console.log("set time is working");
+//   quiz ();
+//   console.log(quiz());
+//   console.log("quiz is working");
   
-}
+// }
 
 // master event listener attached to the start button.
+
 startBtn.addEventListener("click", setTime);
 
 // answer buttons
@@ -100,4 +101,23 @@ function storeTime() {
 
 
 // using local storage to create a score
+
+
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts',
+  },
+  {
+    title: 'Camel Case is a:',
+    choices: ['water bottle', 'way of naming JavaScript elements', 'a police case involving a camel in 1976'],
+    answer: 'way of naming JavaScript elements',
+  },
+  {
+    title: 'How do you call a function?',
+    choices: ['function ();', 'function;', 'function[];', 'function{};'],
+    answer: 'function ()',
+  },
 
