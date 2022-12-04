43bd653b592e9f95a0f0bf2f1a677bd7f7092bc3
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-04 13:58:14 +0800

    working on connecting questions to hmtl

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -3,18 +3,53 @@ var startBtn = document.querySelector(".start-btn");
 var timeEl = document.querySelector(".time");
 var secondsLeft = 100;
 // quiz vars
-// let questions = [(document.querySelector(".quiz-item1")),(document.querySelector(".quiz-item2")), (document.querySelector(".quiz-item3")), (document.querySelector(".quiz-item4"))];
-// console.log(questions);
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice"));
+console.log(choices);
+// what is the currentQuestion
+let currentQuestion = {};
+// what number question is it?
+let questionCounter = 0;
+// how many questions are available?
+let availableQuestions = [];
+// list of all questions, choices, and answers
+let fullQuestions = [
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choice:'strings',
+    choice:'booleans',
+    choice:'alerts',
+    answer: 3,
+  },
+  {
+    title: 'Camel Case is a:',
+    choices: ['water bottle', 'way of naming JavaScript elements', 'a police case involving a camel in 1976'],
+    answer: 'way of naming JavaScript elements',
+  },
+  {
+    title: 'How do you call a function?',
+    choices: ['function ();', 'function[];', 'function{};'],
+    answer: 'function ()',
+  }
+];
+// Incorrect answer results in loss of time - 5s
+const incorrectPenalty = 5;
+
+function question () {
+  questionCounter=0;
+  availableQuestions= [...fullQuestions];
+  getNewQuestion();
+}
+
+function getNewQuestion() {
+  // add question
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() *availableQuestions.length);
+  currentQuestion= availableQuestions[questionIndex];
+  question.innerText=currentQuestion.question;
 
 
 
-
-// initial js stlyes - FIXME:
-// function homeStyle(questions) {
-// for (let i = 0; i < questions.length; i++){
-// questions[i].style.display = 'none'
-// }
-// }
 
 // FIXME: bug casuing it to continue counting after 0. count down timer function
 function setTime() {
@@ -45,68 +80,17 @@ function quiz() {
    
 };
 
-// list of all questions, choices, and answers
-var fullQuestions = [
-  {
-    title: 'Commonly used data types DO NOT include:',
-    choices: ['strings', 'booleans', 'alerts'],
-    answer: 'alerts',
-  },
-  {
-    title: 'Camel Case is a:',
-    choices: ['water bottle', 'way of naming JavaScript elements', 'a police case involving a camel in 1976'],
-    answer: 'way of naming JavaScript elements',
-  },
-  {
-    title: 'How do you call a function?',
-    choices: ['function ();', 'function[];', 'function{};'],
-    answer: 'function ()',
-  }
-];
 
 var questionTitle = fullQuestions[0]["title"];
-var questionOptions1 = Array.from(fullQuestions[0]["choices"]);
+var questionOptions1 = Array.from(document.getElementsByClassName["choice"]);
 var questionAnswer = fullQuestions[0]["answer"];
 //FIXME: from mod4.20 - needs to be for answers not questions.
-function questions() {
- 
-
-  var element = document.querySelector(".question");
-  var element2 = document.querySelector(".options");
-  
-  var state = element.getAttribute("data-state");
-  var state2 = element2.getAttribute("data-state");
-
-  if (state === "hidden"){
-    element.setAttribute("data-state", "visible");
-    element.textContent = questionTitle;
-    element2.setAttribute("data-state", "visible");
-    element2.textContent = questionOptions1;
-   
-  }else {
-    element.setAttribute("data-state", "hidden");
-    element.textContent = "";
-  };
-
-
-  const title = document.createElement();
-  const options = document.createElement();
-  const answer = document.createElement();
-
- var button = document.createElement("BUTTON");
-
-
- button.appendChild(text);
-  // const node = document.createElement(questionTitle);
-
-  // title.appendChild(node);
-  // element.appendChild(title);
-// add for loop - 
 
 
 
-  
-};
+
+
+
 
 // master function - all functions combined for single eventListener.
 function quizTime() {
