1aac9fbb847f850b6693409ce48724095b0e585c
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-04 10:20:06 +0800

    trying to resolve questyion bug

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -5,7 +5,7 @@ var secondsLeft = 100;
 // quiz vars
 // let questions = [(document.querySelector(".quiz-item1")),(document.querySelector(".quiz-item2")), (document.querySelector(".quiz-item3")), (document.querySelector(".quiz-item4"))];
 // console.log(questions);
-console.log(startBtn);
+
 
 
 
@@ -22,92 +22,30 @@ function setTime() {
     secondsLeft--;
     timeEl.textContent = "Timer: " + secondsLeft;
     
-    if (secondsLeft === 0) {
-      clearInterval(timerInterval);
-      timeEl.textContent = "You have run out of time!";
-    }
   }, 1000);
-}
+  if (secondsLeft === 0) {
+    clearInterval(timerInterval);
+    timeEl.textContent = "You have run out of time!";
+  }
+};
 
 // start quiz function - hide welcome header and intro, show quiztime headser and first question.
 function quiz() {
   var intro = document.querySelector(".intro");
   var quiz = document.querySelector(".quiz");
-  
-  
+   
   if (quiz.style.display = 'none');
   intro.style.display = 'none';
   
-  
-  for (let i = 0; i < questions.length; i++) {
-    questions[i].style.display = 'block';
-    
-  }
-  
-}
-console.log(questions[0]);
-
-
-//FIXME: master function - all functions combined for single eventListener.
-// function quizTime () {
-//   setTime ();
-//   console.log(setTime());
-//   console.log("set time is working");
-//   quiz ();
-//   console.log(quiz());
-//   console.log("quiz is working");
-  
-// }
-
-// master event listener attached to the start button.
-
-startBtn.addEventListener("click", setTime);
-
-// answer buttons
-var rightBtn = document.querySelector("#answer-right");
-var wrongBtn = document.querySelector("#answer-wrong");
-
-// right answers colour changed based on validitiy
-function rightAnswerReact(){
-  if (rightBtn.style.backgroundColor === "#0d324d"){
-  
-  rightBtn.style.backgroundColor = '#green';
-  // rightBtn.setProperty.style.color = '--background-color';
-  console.log("reading right answer");
-}
-}
-
-// FIXME:
-rightBtn.addEventListener("click", rightAnswerReact);
-console.log("test");
-// console.log(rightAnswerReact);
-
-
-// wrong answers colour changed based on validitiy
-function wrongAnswerReact(){
-  wrongBtn.setProperty.style.backgroundColor ='--wrong-color';
-  wrongBtn.setProperty.style.color = '--background-color';
-}
-
-// FIXME:
-wrongBtn.addEventListener("click", wrongAnswerReact); 
-
-// local storage - storing end time
-function storeTime() {
-  localStorage.setItem("time", secondsLeft);
-  console.log("store time is working");
-}
-
-
-
-// using local storage to create a score
-
+  if (intro.style.display = 'none');
+  quiz.style.display = 'none';  
+};
 
 // list of all questions, choices, and answers
-var questions = [
+var fullQuestions = [
   {
     title: 'Commonly used data types DO NOT include:',
-    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    choices: ['strings', 'booleans', 'alerts'],
     answer: 'alerts',
   },
   {
@@ -117,29 +55,65 @@ var questions = [
   },
   {
     title: 'How do you call a function?',
-    choices: ['function ();', 'function;', 'function[];', 'function{};'],
+    choices: ['function ();', 'function[];', 'function{};'],
     answer: 'function ()',
   }
-]
+];
 
-
-// from mod4.20 - needs to be for answers not questions.
-function event () {
+//FIXME: from mod4.20 - needs to be for answers not questions.
+function questions() {
  
-  var element = event.target;
+  var element = document.querySelector(".question");
   
   var state = element.getAttribute("data-state");
   
   if (state === "hidden"){
     element.setAttribute("data-state", "visible");
   
-    element.textContent = element.getAttribute("data-number") ;
+    element.textContent = element.getAttribute("data-number");
     
   }else {
     element.setAttribute("data-state", "hidden");
     element.textContent = "";
-  }
+  };
   
+  var questionTitle = fullQuestions[0]["title"]
+  var querstionQuestion = fullQuestions[0]["choices"]
+  var questionAnswer = fullQuestions[0]["answer"]
+
+  const title = document.createElement(h3);
+  const node = document.createElement(questionTitle);
+
+  title.appendChild(node);
+  element.appendChild(title);
+// add for loop - 
+
+  console.log(fullQuestions[0]["title"]);
+
   
-})
+};
+
+// master function - all functions combined for single eventListener.
+function quizTime() {
+  setTime();
+  console.log(setTime());
+  console.log("set time is working");
+  quiz();
+  console.log(quiz());
+  console.log("quiz is working");
+  questions();
+  console.log(questions());
+  console.log("questions is working");
+};
+
+
+
+// using local storage to create a score
+
+
+// master event listener attached to the start button.
+
+
+startBtn.addEventListener("click", quizTime);
+
 
