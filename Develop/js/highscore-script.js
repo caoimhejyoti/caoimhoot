120e84630eaf9ee3c8d7ecee39ba2049a3a3f125
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-07 23:14:24 +0800

    highscore now reveals. now to update table.

--- Develop/js/highscore-script.js
+++ Develop/js/highscore-script.js
@@ -1,16 +1,76 @@
 let finishTime = localStorage.getItem("time");
 let score = document.querySelector("score")
 
-let highScoreOne = document.querySelector("#first-score");
-let highScoreTwo = document.querySelector("#second-score");
-let highScoreThree = document.querySelector("#third-score");
-let highScoreFour = document.querySelector("#fourth-score");
 
-highScoreOne==0;
-highScoreTwo==0;
-highScoreThree==0;
-highScoreFour==0;
 
+let player = ""
+let firstPlace = document.getElementById("first-score");
+let secondPlace = document.getElementById("second-score");
+let thirdPlace = document.getElementById("third-score");
+let fourthPlace = document.getElementById("fourth-score");
+
+let firstInitialEl= document.getElementById("player-inititals-1");
+let secondInitialEl= document.getElementById("player-inititals-2");
+let thirdInitialEl= document.getElementById("player-inititals-3");
+let fourthInitialEl= document.getElementById("player-inititals-4");
+
+let highscoreArray = [
+  
+  {
+    rank: "First Place",
+    score: 0,
+    initial: "",
+  },
+  {
+    rank: "Second Place",
+    score: 0,
+    initial: "",
+  },
+  {
+    rank: "Third Place",
+    score: 0,
+    initial: "",
+  }
+];
+
+currentResult = localStorage.getItem ("currentScore");
+currentPlayer = localStorage.getItem ("player");
+
+currentGame = [
+  {
+    rank: "",
+    score: currentResult,
+    initial: currentPlayer,
+  }
+]
+
+function setHighScore(){
+  console.log("set high score worked");
+  if (currentGame[0].score>=highscoreArray[0].score){
+    let tempScore=highscoreArray[0].score;
+    highscoreArray[0].score=currentGame[0].score;
+    highscoreArray[0].initial=currentGame[0].initial;
+    console.log("if statement reached");
+    firstPlace.innerHTML=currentResult;
+    firstInitialEl.innerHTML=currentPlayer;
+
+
+  }else if (currentGame[1]>=highscoreArray[1][1]){
+    highscoreArray[1][1].value=currentGame[1].value;
+    highscoreArray[1][2].value=currentGame[2].value;
+  }else if (currentGame[1]>=highscoreArray[2][1]){
+    highscoreArray[2][1].value=currentGame[1].value;
+    highscoreArray[2][2].value=currentGame[2].value;
+  }else if (currentGame[1]>=highscoreArray[3][1]){
+    highscoreArray[3][1]=currentGame[1].value;
+    highscoreArray[3][2]=currentGame[2].value;
+  }else
+  document.getElementsByClassName(better-luck).textContent= "Try again to make it onto the Leaderboard!";
+    
+}
+
+
+setHighScore();
 
 
 function createScore() {
