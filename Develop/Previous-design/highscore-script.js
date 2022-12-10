9b5550c44d9741b83f876f28e64f4ace256b8b63
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-10 21:44:30 +0800

    refiling unused documents + addcomments for ref

---
+++ Develop/Previous-design/highscore-script.js
@@ -0,0 +1,36 @@
+//  initial design of quiz separated highscores into independant html, js and css files. 
+
+
+// // let finishTime = localStorage.getItem("time");
+// // let score = document.querySelector("score")
+
+// // let player = ""
+// // let firstPlace = document.getElementById("first-score");
+// // let secondPlace = document.getElementById("second-score");
+// // let thirdPlace = document.getElementById("third-score");
+// // let fourthPlace = document.getElementById("fourth-score");
+
+// // let firstInitialEl= document.getElementById("player-inititals-1");
+// // let secondInitialEl= document.getElementById("player-inititals-2");
+// // let thirdInitialEl= document.getElementById("player-inititals-3");
+// // let fourthInitialEl= document.getElementById("player-inititals-4");
+
+// let olEl = document.getElementById("highscores");
+
+// var highscoreObject = JSON.parse(localStorage.getItem("highscoreArray"));
+// console.log(highscoreObject);
+// let sortedScores = [];
+
+// for (var initital in highscoreObject) {
+//   sortedScores.push([initital,highscoreObject[initital]]);
+// }
+
+// sortedScores.sort((a,b) => {
+//   return b[1] - a[1];
+// });
+
+// for (let i=0; i<sortedScores.length-1; i++) {
+//   liEL = document.createElement("li");
+//   olEl.appendChild(liEL);
+//   liEL.innerHTML = secondsLeft;
+// }
